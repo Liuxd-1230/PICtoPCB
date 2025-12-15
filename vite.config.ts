331a20c2f4a96f3f560a63824c5b5491d5b0677f
Rosemary <128299966+Liuxd-1230@users.noreply.github.com>
2025-12-15 22:48:26 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+    base: '/Liuxd-1230/PICtoPCB/',
       server: {
         port: 3000,
         host: '0.0.0.0',
